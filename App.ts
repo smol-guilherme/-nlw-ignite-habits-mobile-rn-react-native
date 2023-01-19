ee24a31f0dc3497f7528be0cd8da1feb52c2cf44
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2023-01-19 18:01:18 -0300

    feat: added Loading component.

---
+++ App.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#09090A",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#FFFFFF",
+    fontFamily: "Inter_800ExtraBold",
+  },
+});
--- App.tsx
+++ App.tsx
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from "@expo-google-fonts/inter";
+import { Loading } from './src/components/Loading';
+import { styles } from "./App";
+import React from 'react';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold });
+
+  if(!fontsLoaded) {
+    return <Loading />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#09090A',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
