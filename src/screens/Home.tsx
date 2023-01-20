a4879670c409b66993faa98dbdb8532ea835d047
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2023-01-20 18:22:27 -0300

    feat: added weekdays row and dayjs lib.

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,25 @@
-import { View } from "react-native";
+import { Text, View } from "react-native";
+import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
+
+const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 
 export function Home() {
   return (
     <View className="flex-1 bg-background px-8 py-16">
       <Header />
+      <View className="flex-row mt-6 mb-2">
+        {weekDays.map((weekDay, i) => (
+          <Text
+            key={`${weekDay}-${i}`}
+            className="text-zinc-400 font-bold text-xl mx-1 text-center"
+            style={{ width: DAY_SIZE, height: DAY_SIZE }}
+          >
+            {weekDay}
+          </Text>
+        ))}
+      </View>
+      <HabitDay />
     </View>
   );
 }
