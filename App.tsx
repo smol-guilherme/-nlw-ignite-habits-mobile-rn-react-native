9a012e47dbe95ae43ece5445e7102bf5caee5259
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2023-01-20 17:57:06 -0300

    chore: added svg libraries and config files.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,34 @@
-import { Text, View, StatusBar } from 'react-native';
-import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from "@expo-google-fonts/inter";
-import { Loading } from './src/components/Loading';
-import { styles } from "./App";
-import React from 'react';
+import { StatusBar } from "react-native";
+import { Home } from "./src/screens/Home";
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_800ExtraBold,
+} from "@expo-google-fonts/inter";
+import { Loading } from "./src/components/Loading";
 
 export default function App() {
-  const [fontsLoaded] = useFonts({ Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold });
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold,
+  });
 
-  if(!fontsLoaded) {
+  if (!fontsLoaded) {
     return <Loading />;
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Open up App.tsx to start working on your app!</Text>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-    </View>
+    <>
+      <Home />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+    </>
   );
 }
